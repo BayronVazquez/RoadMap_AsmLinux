3712788b4cd062509e2da9c82acde33676df2291
Bayron Vazquez <bayron.vazquez@sandox.com.mx>
2024-01-10 15:45:20 -0600

    1. Hello World

---
+++ 1. Hello World/main.asm
@@ -0,0 +1,22 @@
+; nasm -f elf64 -o main.o main.asm
+; ld -o exe main.o
+
+
+global _start
+
+section .text
+
+_start:
+    mov rax, 1        ; write(
+    mov rdi, 1        ;   STDOUT_FILENO,
+    mov rsi, msg      ;   "Hello, world!\n",
+    mov rdx, msglen   ;   sizeof("Hello, world!\n")
+    syscall           ; );
+
+    mov rax, 60       ; exit(
+    mov rdi, 0        ;   EXIT_SUCCESS
+    syscall           ; );
+
+section .rodata
+    msg: db "Hello, world!", 10
+    msglen: equ $ - msg
